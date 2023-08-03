f4d853d705d79407a364654e4eb7f88f00f3720d
SpiceWeasel66 <nagengasteva@gmail.com>
2023-08-03 20:04:47 +0100

    added JS to update Time

--- JS/java.js
+++ JS/java.js
@@ -1,1 +1,105 @@
-console.log("hello")
+// Los Angeles
+
+function changeTime() {
+  let cityOneDate = moment
+    .tz(`America/Los_Angeles`)
+    .format(`MMMM, [the] Do, YYYY`);
+  document.querySelector("#cityOne .date").innerHTML = cityOneDate;
+
+  let cityOneTime = moment.tz(`America/Los_Angeles`).format(`h:mm:ss`);
+  document.querySelector("#cityOne .time").innerHTML = cityOneTime;
+
+  let cityOneAmPm = moment.tz(`America/Los_Angeles`).format(`A`);
+  document.querySelector("#cityOne .ampm").innerHTML = cityOneAmPm;
+}
+
+changeTime();
+setInterval(changeTime, 900);
+
+//Sao Paulo
+
+function changeTimeTwo() {
+  let cityTwoDate = moment
+    .tz(`America/Bogota`)
+    .format(`MMMM, [the] Do, YYYY`);
+  document.querySelector("#cityTwo .date").innerHTML = cityTwoDate;
+
+  let cityTwoTime = moment.tz(`America/Bogota`).format(`h:mm:ss`);
+  document.querySelector("#cityTwo .time").innerHTML = cityTwoTime;
+
+  let cityTwoAmPm = moment.tz(`America/Bogota`).format(`A`);
+  document.querySelector("#cityTwo .ampm").innerHTML = cityTwoAmPm;
+}
+
+changeTimeTwo();
+setInterval(changeTimeTwo, 900);
+
+//London
+
+function changeTimeThree() {
+  let cityThreeDate = moment.tz(`Europe/London`).format(`MMMM, [the] Do, YYYY`);
+  document.querySelector("#cityThree .date").innerHTML = cityThreeDate;
+
+  let cityThreeTime = moment.tz(`Europe/London`).format(`h:mm:ss`);
+  document.querySelector("#cityThree .time").innerHTML = cityThreeTime;
+
+  let cityThreeAmPm = moment.tz(`Europe/London`).format(`A`);
+  document.querySelector("#cityThree .ampm").innerHTML = cityThreeAmPm;
+}
+
+changeTimeThree();
+setInterval(changeTimeThree, 900);
+
+//Cairo
+
+function changeTimeFour() {
+  let cityFourDate = moment
+    .tz(`Africa/Cairo`)
+    .format(`MMMM, [the] Do, YYYY`);
+  document.querySelector("#cityFour .date").innerHTML = cityFourDate;
+
+  let cityFourTime = moment.tz(`Africa/Cairo`).format(`h:mm:ss`);
+  document.querySelector("#cityFour .time").innerHTML = cityFourTime;
+
+  let cityFourAmPm = moment.tz(`Africa/Cairo`).format(`A`);
+  document.querySelector("#cityFour .ampm").innerHTML = cityFourAmPm;
+}
+
+changeTimeFour();
+setInterval(changeTimeFour, 900);
+
+//Singapore
+
+function changeTimeFive() {
+  let cityFiveDate = moment
+    .tz(`Asia/Singapore`)
+    .format(`MMMM, [the] Do, YYYY`);
+  document.querySelector("#cityFive .date").innerHTML = cityFiveDate;
+
+  let cityFiveTime = moment.tz(`Asia/Singapore`).format(`h:mm:ss`);
+  document.querySelector("#cityFive .time").innerHTML = cityFiveTime;
+
+  let cityFiveAmPm = moment.tz(`Asia/Singapore`).format(`A`);
+  document.querySelector("#cityFive .ampm").innerHTML = cityFiveAmPm;
+}
+
+changeTimeFive();
+setInterval(changeTimeFive, 900);
+
+//Melbourne
+
+function changeTimeSix() {
+  let citySixDate = moment
+    .tz(`Australia/Melbourne`)
+    .format(`MMMM, [the] Do, YYYY`);
+  document.querySelector("#citySix .date").innerHTML = citySixDate;
+
+  let citySixTime = moment.tz(`Australia/Melbourne`).format(`h:mm:ss`);
+  document.querySelector("#citySix .time").innerHTML = citySixTime;
+
+  let citySixAmPm = moment.tz(`Australia/Melbourne`).format(`A`);
+  document.querySelector("#citySix .ampm").innerHTML = citySixAmPm;
+}
+
+changeTimeSix();
+setInterval(changeTimeSix, 900);
